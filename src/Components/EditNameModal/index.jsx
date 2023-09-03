75112a064008f8fda98dd9195af3eb7ce7f5fa5d
goncalo <61824985+Gosamat@users.noreply.github.com>
2023-09-03 19:51:41 +0100

    homepage rebrand

--- src/Components/EditNameModal/index.jsx
+++ src/Components/EditNameModal/index.jsx
@@ -1,42 +1,49 @@
 import React from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+} from "@nextui-org/react";
 
 function EditNameModal(props) {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const {plant} = props
+  const { isOpen, onOpenChange } = props; // Use props to get the modal state
+
+  // Add any other necessary state variables and functions here
+  // For example, if you need to manage input values for the plant name, you can use useState
 
   return (
-      <Modal 
-        isOpen={isOpen} 
-        onOpenChange={onOpenChange}
-        placement="top-center"
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Edit Plant Name</ModalHeader>
-              <ModalBody>
-              <p>Maybe we got the name wrong, or mayber you just want to give it a cool nickname like Bob!</p>
-                <Input
-                  label="Plant Name"
-                  placeholder= {plant.commonName}
-                  type="text"
-                  variant="bordered"
-                  name = "name"
-                />
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Change
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Edit Plant Name</ModalHeader>
+            <ModalBody>
+              {/* Add input fields and logic for editing the plant name */}
+              {/* You can use useState to manage the input value */}
+              <Input
+                label="Plant Name"
+                placeholder={props.selectedPlant.commonName}
+                type="text"
+                variant="bordered"
+                name="name"
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="flat" onPress={onClose}>
+                Close
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                Change
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   );
 }
 
