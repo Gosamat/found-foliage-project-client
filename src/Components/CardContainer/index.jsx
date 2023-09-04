b41c49a5f2445384b7e09edf94df5e41b2256b98
John Doe <marrian1997@hotmail.com>
2023-09-04 17:51:15 +0100

    front page, about page rebrand

--- src/Components/CardContainer/index.jsx
+++ src/Components/CardContainer/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { AuthContext } from "../../Context/Auth.Context";
+
 const boxShadowStyle = {
-    boxShadow: '10px 10px 0px rgba(0, 0, 0, 0.05)',
-  };
-// Function to generate random text
+  boxShadow: '10px 10px 0px rgba(0, 0, 0, 0.05)',
+};
+
 function makeText() {
   const textOptions = [
     'Discover the magic of plant recognition through the lens of your camera.',
@@ -11,78 +14,83 @@ function makeText() {
 
   const randomIndex = Math.floor(Math.random() * textOptions.length);
   return textOptions[randomIndex];
-  
 }
 
+const API_URL = "http://localhost:5005";
+
 const Card = () => {
-  // Retrieve text from localStorage or use a random text if not present
   const initialText = localStorage.getItem('cardText') || makeText();
   const [cardText, setCardText] = useState(initialText);
+  const [plantCollection, setPlantCollection] = useState([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/plant`)
+      .then((response) => {
+        setPlantCollection(response.data);
+      })
+      .catch((error) =>
+        console.log(
+          "error while grabbing plants in user's garden from API: ",
+          error
+        )
+      );
+  }, []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      // Toggle the fade class after 10 seconds
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % plantCollection.length);
       setCardText(makeText());
       setTimeout(() => {
         setCardText(makeText());
-      }, 5000); // Delay for 5 seconds before fading in the new text
+      }, 5000);
 
-      // Store the new text in localStorage
       localStorage.setItem('cardText', cardText);
-    }, 10000); // Change text every 20 seconds (10 seconds for each text)
+    }, 10000);
 
     return () => clearInterval(intervalId);
-  }, [cardText]);
+  }, [cardText, plantCollection]);
 
   const cardData = {
-    imageSrc:
-      'https://raw.githubusercontent.com/Javieer57/CODEPEN-gnarly-grid-cards/main/assets/img-1.png',
     cardTitle: "Nature's Enigma",
   };
 
   return (
-    <li className="card" >
-      <div className="card__thumb" style={boxShadowStyle}>
-        <img
-          className="animate"
-          src={cardData.imageSrc}
-          alt={`Card for ${cardData.cardTitle}`}
-        />
-      </div>
-      <div className="card__content" style={boxShadowStyle}>
-        <h3 className="card__title">{cardData.cardTitle}</h3>
-        <p className={`card__text ${cardText !== initialText ? 'fade' : ''}`}>
-          {cardText}
-        </p>
-        <a
-          className="card__btn"
-          aria-label={`Read more about ${cardData.cardTitle}`}
-          href="#"
-        >
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/4147/4147953.png"
-            alt=""
-          />
-        </a>
-      </div>
-    </li>
-  );
-};
-
-const CardContainer = () => {
-  return (
-    <div>
-      <ul className="cards__container">
-        {/* Render the Card component */}
-        <Card />
-        <div className='text-container'>
-        <h1> Portal to the World of 
-          </h1>
-          <h1>Plant Identification</h1> 
+    <div className="card-container">
+      <li className="card" key={plantCollection._id}>
+        <div className="card__thumb" style={boxShadowStyle}>
+          {plantCollection.length > 0 && (
+            <div className="card__thumb" style={boxShadowStyle}>
+              <img
+                src={plantCollection[currentImageIndex].imgUrl}
+                alt={plantCollection[currentImageIndex].name}
+              />
+            </div>
+          )}
         </div>
-      </ul>
+        <div className="card__content" style={boxShadowStyle}>
+          <h3 className="card__title">{cardData.cardTitle}</h3>
+          <p className={`card__text ${cardText !== initialText ? 'fade' : ''}`}>
+            {cardText}
+          </p>
+          <a
+            className="card__btn"
+            aria-label={`Read more about ${cardData.cardTitle}`}
+            href="#"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/4147/4147953.png"
+              alt=""
+            />
+          </a>
+        </div>
+      </li>
+      <div className='home-page-text'>
+        <h1>Your Title Here</h1>
+      </div>
     </div>
   );
 };
 
-export default CardContainer;
+export default Card;
